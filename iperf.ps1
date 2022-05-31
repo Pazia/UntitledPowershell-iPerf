6271b2156b738567e9990682ebf109e7d8683cb1
Pazia <pazia@live.dk>
2022-05-31 22:37:19 +0200

    Minor tweaks and updates
    1) Clear-Host added.
    2) Section for "Welcome user" has been added. 
    - Text will be added later.
    3) More information for end-user added.
    4) Invoke-Expression for iPerf speed measurement changed slightly.
    - Added download and upload measurement after the variable for each download/upload has been completed to display the speed measurement right away.
    5) End of script text added.

--- iperf.ps1
+++ iperf.ps1
@@ -1,7 +1,7 @@
 <#
-Sidst opdateret d. 25 maj 2022.
+Sidst opdateret d. 31 maj 2022.
 State: Pre-release
-Udviklet af: Jesper Sørensen - jesper@pazia.dk
+Udviklet af: Jesper 'Pazia' Sørensen
 #>
 
 <#
@@ -12,6 +12,7 @@ Roadmap
 - Velkommen/afslutning af scriptet
 - Bed om tilladelse til at uploade oprettet filer til FTP/HTTP-server
 - Flyt log-filerne fra %temp%-mappen, så brugeren har mulighed for at åbne dem
+- Set-Location for at minimere variabler i scriptet
 
 $ispList = 'YouSee', 'Telenor', 'Fibia', 'Senia Network', 'BOLIG:NET', 'TDC Erhverv', 'Hiper', 'Fastspeed', 'Stofa', 'Boxer', 'IP Vision', 'Altibox', 'Cibicom', 'Kviknet'
 
@@ -25,17 +26,18 @@ Opdateringer til scriptet
 
 $ProgressPreference = 'SilentlyContinue' # Suppress progress.
 
-Write-Host "Indlæser script.. Vent et øjeblik. " -NoNewline
-Start-Sleep -Seconds 3
-
-Write-Host "[" -NoNewline
-Write-Host "OK" -ForegroundColor DarkGreen -NoNewline
-Write-Host "]"
+# Greetings fellow mortal. Stay awhile and listen!
+Clear-Host
 
 # TimeStamp for logfile
 function Get-TimeStamp {
   return "{0:dd/MM/yy} {0:HH:mm:ss}" -f (Get-Date)
 }
+
+Write-Host "Variabler indlæst.. " -NoNewline
+Write-Host "[" -NoNewline
+Write-Host "OK" -ForegroundColor DarkGreen -NoNewline
+Write-Host "]"
 
 # Create files needed
 $kviknet = (Get-Date -Format "dd-MM-yyyy_HH-mm-ss") + " " + "kviknet.txt"
@@ -48,12 +50,17 @@ New-Item -Path $env:TEMP -Name deic.txt -ItemType File > $null
 New-Item -Path $env:TEMP -Name fiberby.txt -ItemType File > $null
 New-Item -Path $env:TEMP -Name log.txt -ItemType File > $null
 
-Start-Sleep -Seconds 2
+Start-Sleep -Seconds 1
 
 Rename-Item -Path $env:TEMP\kviknet.txt -NewName $kviknet > $null
 Rename-Item -Path $env:TEMP\deic.txt -NewName $deic > $null
 Rename-Item -Path $env:TEMP\fiberby.txt -NewName $fiberby > $null
 Rename-Item -Path $env:TEMP\log.txt -NewName $log > $null
+
+Write-Host "Log filer oprettet og klar til brug.. " -NoNewline
+Write-Host "[" -NoNewline
+Write-Host "OK" -ForegroundColor DarkGreen -NoNewline
+Write-Host "]"
 
 # Check for iPerf3.exe
 if ((Test-Path $env:TEMP\iperf3.exe -PathType Leaf) -and (Test-Path $env:TEMP\cyg*.dll -PathType Leaf) ) {
@@ -81,7 +88,9 @@ if ((Test-Path $env:TEMP\iperf3.exe -PathType Leaf) -and (Test-Path $env:TEMP\cy
 # Warning if 1 Gbps uplink isn't detected
 Get-NetAdapter -Name "*" -Physical | ForEach-Object {  
     if ($_.LinkSpeed -ne "1 Gbps") { 
-      Write-Warning "Netværkskort opererer ikke med 1 Gbps uplink som forventet."
+      Write-Warning "Netværkskort opererer ikke med 1 Gbps uplink som forventet. Scriptet afbrydes indenfor et par sekunder.."
+      Start-Sleep -Seconds 5
+      Break
     }
     else {
       Write-Host "Kontrollerer netværkskort.. " -NoNewline
@@ -104,54 +113,81 @@ $fiberby_upload = '& $env:TEMP\iperf3.exe -c ookla.kviknet.dk -t 30 -P 10 --logf
 
 # Speed measurement for iPerf
 
-Write-Host "`nPåbegynder hastighedsmålinger.. Vent venligst."
+Write-Host "`nStarter iPerf.. Estimeret ventetid er ca. tre minutter."
+Write-Host "`niPerf resultat via Kviknet:"
 
 Invoke-Expression $kviknet_download
-Invoke-Expression $kviknet_upload
 
-Write-Host "`niPerf resultat via Kviknet:"
-  if(Select-String -path "$env:TEMP\$kviknet" -pattern "iperf Done.")
+if(Select-String -path "$env:TEMP\$kviknet" -pattern "iperf Done.")
 {
     (Get-Content "$env:TEMP\$kviknet" | Select-Object -Index 395) -replace '.+\b(\d.+\sMbits/sec).+', 'Download: $1'
+}
+  else {
+    Write-Host "Fejl!"
+}
+
+Invoke-Expression $kviknet_upload
+
+  if(Select-String -path "$env:TEMP\$kviknet" -pattern "iperf Done.")
+{
     (Get-Content "$env:TEMP\$kviknet" | Select-Object -Index 792) -replace '.+\b(\d.+\sMbits/sec).+', 'Upload: $1'
 }
   else {
-    Write-Host "Fejl observeret under hastighedsmålinger for Kviknet."
+    Write-Host "Fejl!"
 }
 
 Invoke-Expression $deic_dowload
-Invoke-Expression $deic_upload
 
 Write-Host "`niPerf resultat via DeiC:"
   if(Select-String -path "$env:TEMP\$deic" -pattern "iperf Done.")
 {
     (Get-Content "$env:TEMP\$deic" | Select-Object -Index 395) -replace '.+\b(\d.+\sMbits/sec).+', 'Download: $1'
+}
+  else {
+    Write-Host "Fejl!"
+}
+
+Invoke-Expression $deic_upload
+
+if(Select-String -path "$env:TEMP\$deic" -pattern "iperf Done.")
+{
     (Get-Content "$env:TEMP\$deic" | Select-Object -Index 792) -replace '.+\b(\d.+\sMbits/sec).+', 'Upload: $1'
 }
   else {
-    Write-Host "Fejl observeret under hastighedsmålinger for DeiC."
+    Write-Host "Fejl!"
 }
 
-Invoke-Expression $fiberby_dowload
-Invoke-Expression $fiberby_upload
-
 Write-Host "`niPerf resultat via Fiberby:"
+
+Invoke-Expression $fiberby_dowload
+
   if(Select-String -path "$env:TEMP\$fiberby" -pattern "iperf Done.")
 {
     (Get-Content "$env:TEMP\$fiberby" | Select-Object -Index 395) -replace '.+\b(\d.+\sMbits/sec).+', 'Download: $1'
+}
+  else {
+    Write-Host "Fejl!"
+}
+
+Invoke-Expression $fiberby_upload
+
+if(Select-String -path "$env:TEMP\$fiberby" -pattern "iperf Done.")
+{
     (Get-Content "$env:TEMP\$fiberby" | Select-Object -Index 792) -replace '.+\b(\d.+\sMbits/sec).+', 'Upload: $1'
 }
   else {
-    Write-Host "Fejl observeret under hastighedsmålinger for Fiberby."
+    Write-Host "Fejl!"
 }
 
 # iPerf speed measurements completed
 
-# Get Time/Date, NIC and Traceroute informations with timestamps
+# NIC informations
 Write-Host "`nIndhenter oplysninger om netværkskortet.. " -NoNewline
 
 Add-Content -Path "$env:TEMP\$log" -Encoding UTF8 -Value "Udført d. $(Get-TimeStamp)" -PassThru > $null
 Get-NetAdapter -Name "*" | Format-Table Name, InterfaceDescription, VlanID, LinkSpeed, MediaConnectionState | Out-File -Encoding UTF8 -FilePath "$env:TEMP\$log" -Append
+
+Start-Sleep -Seconds 1
 
 Write-Host "[" -NoNewline
 Write-Host "OK" -ForegroundColor DarkGreen -NoNewline
@@ -160,6 +196,7 @@ Write-Host "]"
 # Move .txt files - STILL IN PROGRESS
 
 # End of script
-Write-Host "Tak for nu!"
+Write-Host "Scriptet er nu udført.."
+Write-Host "Enhver form for feedback kan ske via www.github.com/pazia"
 
 Break
